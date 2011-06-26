38cec16e1d3b3ea7d4f76f4cf3bb9f99b0acd5dd
SiegeLord <slabode@aim.com>
2011-06-26 12:32:28 -0400

    Forgot to add the color_ret file

---
+++ allegro5/allegro_color_ret.d
@@ -0,0 +1,13 @@
+module allegro5.allegro_color_ret;
+
+import allegro5.allegro;
+
+extern(C)
+{
+	ALLEGRO_COLOR al_color_yuv(float y, float u, float v);
+	ALLEGRO_COLOR al_color_cmyk(float c, float m, float y, float k);
+	ALLEGRO_COLOR al_color_hsl(float h, float s, float l);
+	ALLEGRO_COLOR al_color_hsv(float h, float s, float v);
+	ALLEGRO_COLOR al_color_name(in char* name);
+	ALLEGRO_COLOR al_color_html(in char* string);
+}
