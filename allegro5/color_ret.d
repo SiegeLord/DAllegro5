dc8d8453327d30309c9003de1c44a7ff98c75c08
SiegeLord <slabode@aim.com>
2011-08-20 22:48:48 -0300

    Fixed a typo. Thanks to Simon for the patch.

--- allegro5/color_ret.d
+++ allegro5/color_ret.d
@@ -7,7 +7,7 @@ extern(C)
 {
 	ALLEGRO_COLOR al_get_pixel(ALLEGRO_BITMAP* bitmap, int x, int y);
 	/* Pixel mapping */
-	ALLEGRO_COLOR al_map_rgb(char r, ubyte g, ubyte b);
+	ALLEGRO_COLOR al_map_rgb(ubyte r, ubyte g, ubyte b);
 	ALLEGRO_COLOR al_map_rgba(ubyte r, ubyte g, ubyte b, ubyte a);
 	ALLEGRO_COLOR al_map_rgb_f(float r, float g, float b);
 	ALLEGRO_COLOR al_map_rgba_f(float r, float g, float b, float a);
