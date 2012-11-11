6e25f258f9a75d86cd6d5121e1289a96431d8e31
Leandro Motta Barros <lmb@stackedboxes.org>
2012-11-11 17:26:37 -0200

    Fixed signature of al_compose_transform().

--- allegro5/transformations.d
+++ allegro5/transformations.d
@@ -26,7 +26,7 @@ extern (C)
 	void al_scale_transform(ALLEGRO_TRANSFORM* trans, float sx, float sy);
 	void al_scale_transform_3d(ALLEGRO_TRANSFORM *trans, float sx, float sy, float sz);
 	void al_transform_coordinates(in ALLEGRO_TRANSFORM* trans, float* x, float* y);
-	void al_compose_transform(in ALLEGRO_TRANSFORM* trans, ALLEGRO_TRANSFORM* trans2);
+	void al_compose_transform(ALLEGRO_TRANSFORM* trans, in ALLEGRO_TRANSFORM* trans2);
 	ALLEGRO_TRANSFORM* al_get_current_transform();
 	const_ALLEGRO_TRANSFORM* al_get_current_inverse_transform();
 	void al_invert_transform(ALLEGRO_TRANSFORM* trans);
