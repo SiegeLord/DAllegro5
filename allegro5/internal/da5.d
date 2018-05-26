f88d64db907afe79dced1700873d5dd9892a3751
Simon Naarmann <s.naarmann@gmail.com>
2018-05-26 22:06:30 +0200

    Fix #41: al_get_pixel is nothrow @nogc
    All generated functions from this mixin should be nothrow @nogc.
    Issue #41 suggested that al_get_pixel get these annotations,
    but al_get_pixel is not the only function that needs them.

--- allegro5/internal/da5.d
+++ allegro5/internal/da5.d
@@ -3,7 +3,7 @@ module allegro5.internal.da5;
 char[] ColorWrapper(in char[] prefix, in char[] func, in char[] arg_decls, in char[] arg_names)
 {
 	return
-	`ALLEGRO_COLOR ` ~ func ~ `(` ~ arg_decls ~ `)
+	`nothrow @nogc ALLEGRO_COLOR ` ~ func ~ `(` ~ arg_decls ~ `)
 	{
 		auto ret = ` ~ prefix ~ func ~ `(` ~ arg_names ~ `);
 		version(ALLEGRO_SUB)
